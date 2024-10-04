89c57706630270a34b8182cb97bb7da60727e903
Esra Ünal <119916520+esralkann@users.noreply.github.com>
2024-10-04 03:08:36 +0300

    created WepApi project and migration for db

--- Infrastructure/CarBookApp,Persistance/Context/CarBookAppContext.cs
+++ Infrastructure/CarBookApp,Persistance/Context/CarBookAppContext.cs
@@ -12,7 +12,18 @@ namespace CarBookApp_Persistance.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=DESKTOP-28MCRVV;initial Catalog=CarBookApDb;integrated Security=true;");
+            optionsBuilder.UseSqlServer("Server=DESKTOP-28MCRVV;initial Catalog=CarBookApDb;integrated Security=true;TrustServerCertificate=true;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CarPricing>(entity =>
+            {
+                entity.Property(e => e.Amount)
+                      .HasColumnType("decimal(18,2)");
+            });
         }
 
         public DbSet<About> Abouts { get; set; }
