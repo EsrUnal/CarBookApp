4090422f95cbd632a2d5e6b37f7583ad8addee32
Esra Ünal <119916520+esralkann@users.noreply.github.com>
2024-10-07 23:31:27 +0300

    CQRS design pattern foundations created

---
+++ Core/CarBookApp.Application/Features/CQRS/Handlers/AboutHandlers/UpdateAboutCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using CarBookApp.Application.Features.CQRS.Commands.AboutCommands;
+using CarBookApp.Application.Interfaces;
+using CarBookApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookApp.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class UpdateAboutCommandHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public UpdateAboutCommandHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateAboutCommand command)
+        {
+            var value = await _repository.GetByIdAsync(command.AboutID);
+            value.Title = command.Title;
+            value.Description = command.Description;
+            value.ImageUrl = command.ImageUrl;
+            await _repository.UpdateAsync(value);
+        }
+    }
+}
